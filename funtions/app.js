5a04d0c317f8769d029d009c93b6a55e17aae9ba
RadwanAnik <ahmedradwan052@gmail.com>
2022-08-23 21:47:02 +0600

    functiona block

---
+++ funtions/app.js
@@ -0,0 +1,36 @@
+const myText = 'Hello World';
+const newString = myText.replace('World', 'Javascript');
+console.log(newString)
+
+
+const arraySentence = ['Learning', 'Javascript', 'is', 'most', 'important', 'as', 'Developer'];
+const completeSentence = arraySentence.join(' ');
+console.log(completeSentence)
+
+
+const name = 'Zaptec';
+function greeting() {
+    console.log(`Our company is called ${name}.`);
+}
+greeting();
+
+
+
+const z = 3;
+
+function a() {
+    const x = 1;
+    output(x)
+}
+
+function b() {
+    const y = 2;
+    output(y)
+}
+
+function output(v) {
+    console.log(v)
+}
+output(z)
+a()
+b()
---
+++ funtions/app2.js
@@ -0,0 +1,5 @@
+const name = 'Chris';
+function greeting() {
+    console.log(`Hello ${name}: welcome to our company.`);
+}
+greeting()
